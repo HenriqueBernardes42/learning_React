96a2c5baff10189f06105605355c9d6afe4de26d
Henrique Bernardes <henriquepabe@gmail.com>
2021-11-24 20:49:39 -0300

    pasta components criada

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import HelloWorld from "./components/HelloWorld"
+
+const name = "Henrique"
+const soma = 2 + 22
+function calcula(a,b){
+  return a * b
+}
+
+const url = 'https://via.placeholder.com/150'
 function App() {
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>paragrafo</p>
-      <p>outro paragrafo</p>
-      <h2>assim fica um pouco mais facil</h2>
-      <h3></h3>
+      <h1>Olá {name}</h1>
+      <p>a soma é {soma}</p>
+      <p>{calcula(2,5)}</p>
+      <img src={url} alt="djfj" />
+      <HelloWorld />
     </div>
   );
 }
