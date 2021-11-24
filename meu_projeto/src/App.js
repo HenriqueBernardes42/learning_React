181f13ad27eea6390006bcfe14f754cd6f5fcb9f
Henrique Bernardes <henriquepabe@gmail.com>
2021-11-24 02:01:12 -0300

    feitas as primeiras modificações

--- meu_projeto/src/App.js
+++ meu_projeto/src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React</h1>
+      <p>paragrafo</p>
+      <p>outro paragrafo</p>
+      <h2>assim fica um pouco mais facil</h2>
+      <h3></h3>
     </div>
   );
 }
